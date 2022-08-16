0539922b649aa617f346b4e76ea04df5128f88c9
Rudranu Palit <rudranupalit13@gmail.com>
2022-08-15 23:43:24 -0500

    most common word py

---
+++ mostcommonword.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        paragraph=re.sub(r'[,.!?;\']',' ',paragraph)
+        paragraph = paragraph.lower()
+        s=paragraph.split()
+        count=0
+        word=""
+        for i in list(set(s)):
+            if s.count(i)>count and i not in banned:
+                count=s.count(i)
+                word =i
+        return word
